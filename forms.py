d28c07b85a68432cb33b42e862ca04fcf84e12df
jayC02 <98602035+jayC02@users.noreply.github.com>
2022-12-12 14:17:37 +0000

    Test

---
+++ forms.py
@@ -0,0 +1,106 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, DateField, DateTimeField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
+class CommentForm(FlaskForm):
+    body = TextAreaField('Wanna say something?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=200)])
+    profilepic = FileField(validators=[
+                            FileAllowed(['png'], 'Only png!') ])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, original_email, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+        self.original_email = original_email
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user:
+                raise ValidationError('TOO LATE, that username has already been chosen, loser, chose another one!')
+
+    def validate_email(self, email):
+        if email.data != self.original_email:
+            user = User.query.filter_by(email=self.email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    profilepic = FileField(validators=[
+                            FileRequired(),
+                            FileAllowed(['png'], 'Only png!') ])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+class PostForm(FlaskForm):
+    title = StringField('Event title', validators=[DataRequired()])
+    date = DateField('Event date dd-mm-yyyy', format='%d-%m-%Y')
+    time = DateTimeField('Time of the event HH:MM', format='%H:%M')
+    venue = StringField('Where is the event taking place?', validators=[DataRequired()])
+    post = TextAreaField('What is the event about?', validators=[DataRequired()])
+    eventphoto = FileField('Event photo', validators=[
+                            FileRequired(),
+                            FileAllowed(['png'], 'Only png!') ])
+    submit = SubmitField('Submit')
+
+class EditPostForm(FlaskForm):
+    title = StringField('Event title', validators=[DataRequired()])
+    date = DateField('Event date dd-mm-yyyy', format='%d-%m-%Y')
+    time = DateTimeField('Time of the event', format='%H:%M')
+    venue = StringField('Where is the event taking place?', validators=[DataRequired()])
+    post = TextAreaField('What is the event about?', validators=[DataRequired()])
+    eventphoto = FileField('Event photo', validators=[
+                            FileRequired(),
+                            FileAllowed(['png'], 'Only png!') ])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_title, original_date, original_time,
+                 original_venue, original_post, *args, **kwargs):
+        super(EditPostForm, self).__init__(*args, **kwargs)
+        self.original_title = original_title
+        self.original_date = original_date
+        self.original_time = original_time
+        self.original_venue = original_venue
+        self.original_post = original_post
+
+class ResetP(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
+
+class PrivateMessages(FlaskForm):
+    message = TextAreaField('Message', validators=[
+        DataRequired(), Length(min=0, max=140)])
+    submit = SubmitField('Submit')
